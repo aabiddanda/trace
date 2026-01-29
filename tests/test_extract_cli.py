56dab3bfca0f86bbccc7d8554faf9ab805100a69
Arjun Biddanda <aabiddanda@gmail.com>
2026-01-29 17:11:58 -0500

    test: included bad bed files

--- tests/test_extract_cli.py
+++ tests/test_extract_cli.py
@@ -42,6 +42,15 @@ def proper_chr1_regions(tmp_path_factory):
         out.write("chr1\t2000000\t4000000\n")
     return fn
 
+@pytest.fixture(scope="session")
+def bad_chr_regions(tmp_path_factory):
+    """Create a dumb file ..."""
+    fn = tmp_path_factory.mktemp("extract_data") / "chr1_regions.txt"
+    with open(fn, "w+") as out:
+        out.write("chrX\t0\t1000000\tX\n")
+        out.write("chrX\t2000000\t1500000\tY\n")
+    return fn
+
 
 def check_npz_file(fp):
     """Check an extracted npz file."""
@@ -144,5 +153,14 @@ def test_chrom_mismatch(tszip1, proper_chr1_regions, c):
     outfix = Path(tszip1).with_suffix("")
     exit_status = os.system(
         f"trace-extract --tree-file {tszip1} --individuals 0,1,2 --chrom {c} --include_regions {proper_chr1_regions} --out {outfix}"
+    )
+    assert exit_status != 0
+
+def test_chrom_regions(tszip1, bad_chr_regions):
+    """Test defining chromosomal regions."""
+    out_fp = Path(tszip1).with_suffix(".npz")
+    outfix = Path(tszip1).with_suffix("")
+    exit_status = os.system(
+        f"trace-extract --tree-file {tszip1} --individuals 0,1,2 --chrom chr1 --include-regions {bad_chr_regions} --out {outfix}"
     )
     assert exit_status != 0
